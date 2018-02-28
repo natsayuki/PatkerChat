05a8529e4d4572dc172d1e4fedb354fa9026817e
the42ndturtle <the42ndturtle@gmail.com>
2018-02-28 18:54:49 -0500

    throw

--- index.js
+++ index.js
@@ -49,6 +49,7 @@ io.on('connection', (socket) => {
     console.log(json);
     let sql = 'SELECT * FROM users WHERE username = ' + json['username'];
     con.query(sql, function(err, result){
+      if(err) throw err;
       console.log(result);
     });
   });
